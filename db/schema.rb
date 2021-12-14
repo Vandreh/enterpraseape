d3980ae25ffcdffe121254dce594f14a3609ae17
Vandreh Esmeraldo <esmeraldovandreh@gmail.com>
2021-12-14 15:47:38 -0300

    adding-purchases

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_13_212250) do
+ActiveRecord::Schema.define(version: 2021_12_14_175828) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 2021_12_13_212250) do
     t.integer "employee_id"
   end
 
+  create_table "purchases", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.integer "quantity"
+    t.integer "invoice_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["invoice_id"], name: "index_purchases_on_invoice_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -59,4 +69,5 @@ ActiveRecord::Schema.define(version: 2021_12_13_212250) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "purchases", "invoices"
 end
